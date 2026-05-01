c88dc055f27dedc5e4d35ddc98994b052910230e
Ravindu Fernando <devminafdo@gmail.com>
2026-05-01 22:44:56 +0530

    First part of the pattern code

---
+++ Hi.py
@@ -0,0 +1,10 @@
+import turtle
+
+# Setup screen
+screen = turtle.Screen()
+screen.bgcolor("black")
+
+# Create turtle
+pen = turtle.Turtle()
+pen.speed(0)  # Fastest speed
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
