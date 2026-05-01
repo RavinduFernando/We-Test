151a7b2921760d27cab78e0c69c16df973e02bdf
Ravindu Fernando <devminafdo@gmail.com>
2026-05-01 22:52:37 +0530

    First part another edit

--- Hi.py
+++ Hi.py
@@ -7,3 +7,16 @@ screen.bgcolor("black")
 # Create turtle
 pen = turtle.Turtle()
 pen.speed(0)  # Fastest speed
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+
+#Drew "HI"
+pen.penup()         
+pen.goto(-100, 0)
+pen.pendown()   
+pen.color(colors[0])
+pen.write("HI", font=("Arial", 48, "bold"))
+
+
+Delay = 2000  # Delay in milliseconds
+turtle.done()   
+
